495fb1b087c81a4068a031df7ae5c722d819e5f8
hausu <kennedymj2@gmail.com>
2019-06-09 17:30:56 -0500

    fixed radio buttons on answer forms

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,35 +3,35 @@
 $(document).ready(function (){
 
     // game starts when user clicks the start button
-    $("#start").on("click", gameState.startTimer);
+    $("#start").on("click", gameTimer.startTimer);
 
 })
 
-// object containing functions to display time 
-var gameState = {
+// object containing method functions to display time and start the game
+var gameTimer = {
 
     // set timer at 60 seconds
     timeRemaining : 10,
 
     // start the timer, begin game
     startTimer: function(){
-        $("#timer").text("Time Remaining: " + gameState.timeRemaining);
+        $("#timer").text("Time Remaining: " + gameTimer.timeRemaining);
         // count down by 1 second
-        setInterval(gameState.countdown, 1000);
+        setInterval(gameTimer.countDown, 1000);
         // hides the start button so start can't be pressed again
         $("#start-screen").hide();
         // calls function to display trivia
-        trivia.displayQuestions();
+        triviaGame.displayQuestions();
     },
     // Update the timer on the UI
-    countdown: function(){
+    countDown: function(){
         // decrement time remaining
-        gameState.timeRemaining--;
+        gameTimer.timeRemaining--;
         // updates html with the time remaining
-        $("#timer").text("Time Remaining: " + gameState.timeRemaining);
+        $("#timer").text("Time Remaining: " + gameTimer.timeRemaining);
         // stops and removes the timer at zero 
-        if(gameState.timeRemaining === 0){
-            gameState.stopTimer();
+        if(gameTimer.timeRemaining === 0){
+            gameTimer.stopTimer();
         }
     },
     // stop timer check answers
@@ -42,15 +42,15 @@ var gameState = {
         $("#timer").empty();
         // hides the timer because otherwise negatives are displayed
         $("#timer").hide();
-        // trivia.checkAnswers();
+        // triviaGame.checkAnswers();
     },
 }
 
-var trivia = {
+var triviaGame = {
 
     // displays trivia questions and answers on our html
     displayQuestions: function() {
-        // variable to append questions and answers to the html
+        // variable to grab our html 
         var output = $("#questions-box");
         // updates html with headline to guide users
         output.append('<h2>Answer the following questions:</h2>');
@@ -61,21 +61,22 @@ var trivia = {
             // updates our html with the trivia question from our myQuestions object
             output.append('<div id="question">' + myQuestions[i].question + '</div>');
 
-            // variables to house each potential answer
+            // variables to house each answer option
             var answer1 = myQuestions[i].answers[0];
             var answer2 = myQuestions[i].answers[1];
             var answer3 = myQuestions[i].answers[2];
 
             // updates the html with our answers and radio buttons so only one can be selected 
-            output.append('<div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><label class="form-check-label" for="inlineRadio1">' + answer1 + '</label></div>');
-            output.append('<div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><label class="form-check-label" for="inlineRadio1">' + answer2 + '</label></div>');
-            output.append('<div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><label class="form-check-label" for="inlineRadio1">' + answer3 + '</label></div>');
-
-
-            
-            
+            output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer1 + '</label></div>');
+            output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer2 + '</label></div>');
+            output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer3 + '</label></div>');
         }
-    }
+
+        var submitBtn = $('<br><button class="btn" id="submit-button">Submit</button>')
+        output.append(submitBtn);
+        $("#submit-button").on("click", gameTimer.stopTimer);
+    },
+
 }
 
 
@@ -85,5 +86,16 @@ var myQuestions = [
         question: "What movie series inspired the inclusion of the chainsaw as a weapon?", 
         answers: ["Evil Dead", "Texas Chainsaw Massacre", "American Psycho"],
         correctAnswer: "Evil Dead",
-    }
+    },
+    {
+        question: "From what movie does the title 'DOOM' come from?",
+        answers: ["War of the Worlds", "Total Recall", "The Color of Money"],
+        correctAnswer: "The Color of Money",
+    },
+    {
+        question: "while in development the game that would eventually become Doom was briefy considered to be based on what movie?",
+        answers: ["Die Hard", "Aliens", "The Terminator"],
+        correctAnswer: "Aliens",
+    },
+    
 ] 
