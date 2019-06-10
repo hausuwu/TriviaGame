8052c8642963ca96da464cf6a4699c27a9f60f3d
hausu <kennedymj2@gmail.com>
2019-06-09 19:57:41 -0500

    organized functions divided into commented sequencial sections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,87 @@
 
-
-$(document).ready(function (){
+// loads entire page
+$(document).ready(function () {
 
     // game starts when user clicks the start button
-    $("#start").on("click", gameTimer.startTimer);
+    $("#start").on("click", startTimer);
 
-})
+});
 
-// object containing method functions to display time and start the game
-var gameTimer = {
+// ------------------- Questions -------------------
 
-    // set timer at 60 seconds
-    timeRemaining : 10,
+// Array holds trivia questions and answers
+var myQuestions = [
+    {
+        question: "What movie series inspired the inclusion of the chainsaw as a weapon?", 
+        answers: ["Evil Dead", "Texas Chainsaw Massacre", "American Psycho", "Hellraiser"],
+        correctAnswer: "Evil Dead",
+    },
+    {
+        question: "From what movie does the title 'DOOM' come from?",
+        answers: ["War of the Worlds", "Total Recall", "The Color of Money", "Apocalypse Now"],
+        correctAnswer: "The Color of Money",
+    },
+    {
+        question: "while in development the game that would eventually become Doom was briefy considered to be based on what movie?",
+        answers: ["Die Hard", "Aliens", "The Terminator", "Robocop"],
+        correctAnswer: "Aliens",
+    },
 
+] 
+
+// ------------------- Global Variables -------------------
+
+// set game timer clock to 60 seconds
+var timeRemaining = 60;
+var correct;
+var userAnswer;
+var numCorrect = 0;
+var numIncorrect = 0;
+var numUnanswered = 0;
+
+
+
+// ------------------- Game Timer -------------------
+    
     // start the timer, begin game
-    startTimer: function(){
-        $("#timer").text("Time Remaining: " + gameTimer.timeRemaining);
+    function startTimer() {
+        $("#timer").text("Time Remaining: " + timeRemaining);
         // count down by 1 second
-        setInterval(gameTimer.countDown, 1000);
+        setInterval(countDown, 1000);
         // hides the start button so start can't be pressed again
         $("#start-screen").hide();
         // calls function to display trivia
-        triviaGame.displayQuestions();
-    },
+        displayQuestions();
+    }
+
     // Update the timer on the UI
-    countDown: function(){
+    function countDown() {
         // decrement time remaining
-        gameTimer.timeRemaining--;
+        timeRemaining--;
         // updates html with the time remaining
-        $("#timer").text("Time Remaining: " + gameTimer.timeRemaining);
+        $("#timer").text("Time Remaining: " + timeRemaining);
         // stops and removes the timer at zero 
-        if(gameTimer.timeRemaining === 0){
-            gameTimer.stopTimer();
+        if(timeRemaining === 0){
+            stopTimer();
         }
-    },
+    }
+
     // stop timer check answers
-    stopTimer: function(){
+    function stopTimer() {
 
         clearInterval();
         // clears the timer from the html
         $("#timer").empty();
         // hides the timer because otherwise negatives are displayed
         $("#timer").hide();
-        // triviaGame.checkAnswers();
-    },
-}
+        // checkAnswers();
+    }
 
-var triviaGame = {
+
+// ------------------- Display Questions -------------------
 
     // displays trivia questions and answers on our html
-    displayQuestions: function() {
+    function displayQuestions() {
         // variable to grab our html 
         var output = $("#questions-box");
         // updates html with headline to guide users
@@ -65,37 +97,45 @@ var triviaGame = {
             var answer1 = myQuestions[i].answers[0];
             var answer2 = myQuestions[i].answers[1];
             var answer3 = myQuestions[i].answers[2];
+            var answer4 = myQuestions[i].answers[3];
+
 
             // updates the html with our answers and radio buttons so only one can be selected 
             output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer1 + '</label></div>');
             output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer2 + '</label></div>');
             output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer3 + '</label></div>');
+            output.append('<div class="form-check form-check-inline mb-4"><input class="form-check-input" type="radio" name="radio '+i+' group" id="radio '+i+'" <label class="form-check-label" id="radio '+i+' label" for="radio '+i+'">' + answer4 + '</label></div>');
+
         }
 
         var submitBtn = $('<br><button class="btn" id="submit-button">Submit</button>')
         output.append(submitBtn);
-        $("#submit-button").on("click", gameTimer.stopTimer);
-    },
+        $("#submit-button").on("click", stopTimer);
+    }
 
-}
+    function checkAnswers() {
+        
+        for(var i = 0; i < myQuestions.length; i++) {
+            correct = myQuestions[i].correctAnswer;
+            userAnswer = $('input[id=radio'+i+']:checked + label').text();
+
+            if (userAnswer === correct) {
+                numCorrect++;
+            }
+            else if (userAnswer !== correct) {
+                numIncorrect++;
+            }
+            else if ( userAnswer === "") {
+                numUnanswered++;
+            }
+        }
+    } 
+
+// ------------------- End Game -------------------
+
+    function scoreScreen() {
+        $("#question-box").empty();
+    }
 
 
-// object holds trivia questions and answers
-var myQuestions = [
-    {
-        question: "What movie series inspired the inclusion of the chainsaw as a weapon?", 
-        answers: ["Evil Dead", "Texas Chainsaw Massacre", "American Psycho"],
-        correctAnswer: "Evil Dead",
-    },
-    {
-        question: "From what movie does the title 'DOOM' come from?",
-        answers: ["War of the Worlds", "Total Recall", "The Color of Money"],
-        correctAnswer: "The Color of Money",
-    },
-    {
-        question: "while in development the game that would eventually become Doom was briefy considered to be based on what movie?",
-        answers: ["Die Hard", "Aliens", "The Terminator"],
-        correctAnswer: "Aliens",
-    },
-    
-] 
+
